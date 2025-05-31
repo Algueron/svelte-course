e089f96a4c609b6f3d85ae57ec9fd0b94686fd6f
Julien Papillon <julien.papillon@gmail.com>
2025-05-31 16:00:40 +0200

    feat: Make our component reactive

--- src/lib/components/DisplayName.svelte
+++ src/lib/components/DisplayName.svelte
@@ -1,11 +1,24 @@
 <script lang="ts">
-  let name = 'Julien Papillon'
+  let firstName = $state('Julien')
+  let lastName = $state('Papillon')
+  let fullName = $derived.by(() => {
+    return `${firstName} ${lastName}`
+  })
+  let username = $state('jpapillon')
   let src = 'https://picsum.photos/250/300'
   let bio = 'Hello, <b>this</b> is me.'
+
+  $effect(() => {
+    if(username || fullName)
+    console.log('User has a username or a full name')
+  })
 </script>
 
-<img {src} alt="A photo of {name}">
-<h1>{name}</h1>
+<input bind:value={username} />
+<input bind:value={firstName} />
+<input bind:value={lastName} />
+<!--<img {src} alt="A photo of {name}">>-->
+<h1>{username || fullName}</h1>
 <p>{@html bio}</p>
 
 <style>
