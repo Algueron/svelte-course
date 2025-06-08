c59cdf82580f0c428f2c33d832771b777e21228b
Julien Papillon <julien.papillon@gmail.com>
2025-06-08 12:13:38 +0200

    feat: create counter component

---
+++ src/lib/components/Counter.svelte
@@ -0,0 +1,24 @@
+<script>
+    let count = $state(0);
+    let frequency = $state(1000);
+
+    $effect(() => {
+        const interval = setInterval(() => {
+            console.log(count);
+            count += 1;
+        }, frequency);
+        console.log('intervalid', interval);
+        return () => {
+            clearInterval(interval);
+        }
+    })
+</script>
+
+<h1>{count}</h1>
+
+<button onclick={() => {
+    frequency *= 2;
+}}>Slower</button>
+<button onclick={() => {
+    frequency /= 2;
+}}>Faster</button>
