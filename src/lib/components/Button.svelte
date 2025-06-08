efb79fcdaf302fbe51226e03aba19b47b5efc6ec
Julien Papillon <julien.papillon@gmail.com>
2025-06-08 17:59:18 +0200

    feat: Initialize button component

---
+++ src/lib/components/Button.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+
+</script>
+
+{#snippet sum(a: number, b: number)}
+    {#snippet bold(x: any)}
+        <b>{x}</b>
+    {/snippet}
+    <span>{a} + {b} = {@render bold(a + b)}</span>
+{/snippet}
+
+<button>Text</button>
+
+{@render sum(2,2)}
+
+<style>
+    button {
+        border: none;
+        background-color: #ff3e00;
+        color: #ffffff;
+        padding: 0 20px;
+        height: 45px;
+        font-weight: bold;
+        border-radius: 5px;
+        cursor: pointer;
+    }
+</style>
