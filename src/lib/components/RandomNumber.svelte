a6223158f92b26182aa2168bc61ddb161cf28f01
Julien Papillon <julien.papillon@gmail.com>
2025-06-08 10:46:24 +0200

    feat: Create random number component

---
+++ src/lib/components/RandomNumber.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    let randomNumber = $state(Math.floor(Math.random() * 10));
+    let doubleRandomNumber = $derived(randomNumber * 2);
+</script>
+
+<h2>The random number is: {randomNumber}</h2>
+<h2>Double random number is: {doubleRandomNumber}</h2>
+
+<button onclick={() => {
+    randomNumber = Math.floor(Math.random() * 10);
+    console.log({ randomNumber, doubleRandomNumber});
+}}>Generate</button>
