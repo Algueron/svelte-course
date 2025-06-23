19e7f6f95ec2164be3b3550b971d20449ff073cb
Julien Papillon <julien.papillon@gmail.com>
2025-06-23 20:51:23 +0200

    feat: Extract Counter logic into function

---
+++ src/lib/utils/counter.svelte.ts
@@ -0,0 +1,17 @@
+function createCounter() {
+    let count = $state(0);
+    
+    return {
+        get value() {
+            return count;
+        },
+        increment: () => {
+            count += 1;
+        }, 
+        reset: () => {
+            count = 0;
+        }
+    }
+}
+
+export default createCounter;
