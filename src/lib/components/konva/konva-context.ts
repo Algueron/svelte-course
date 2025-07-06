44364236c2517f2155ae3a579458baee17f96d3c
Julien Papillon <julien.papillon@gmail.com>
2025-07-06 10:59:51 +0200

    feat: Implement Konva Rectangle as native Svelte component

--- src/lib/components/konva/konva-context.ts
+++ src/lib/components/konva/konva-context.ts
@@ -2,6 +2,7 @@ import Konva from "konva";
 import { getContext, setContext } from "svelte";
 
 const stageKey = Symbol('konva-stage');
+const layerKey = Symbol('konva-layer');
 
 export function setStageContext(getStage: () => Konva.Stage) {
     setContext(stageKey, getStage);
@@ -15,4 +16,16 @@ export function getStageContext() {
     }
     const stage = getStage();
     return stage;
+}
+
+export function setLayerContext(layer: Konva.Layer) {
+    setContext(layerKey, layer);
+}
+
+export function getLayerContext() {
+    const layer = getContext<Konva.Layer>(layerKey);
+    if(!layer) {
+        throw new Error('A component must have a Layer as a parent.');
+    }
+    return layer;
 }
