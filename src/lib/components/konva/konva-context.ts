67c7dc61c4312b0ff8ae12b2d7dd66dcd8c3eb68
Julien Papillon <julien.papillon@gmail.com>
2025-07-06 10:44:27 +0200

    feat: Implement Konva Layer as native Svelte component

---
+++ src/lib/components/konva/konva-context.ts
@@ -0,0 +1,18 @@
+import Konva from "konva";
+import { getContext, setContext } from "svelte";
+
+const stageKey = Symbol('konva-stage');
+
+export function setStageContext(getStage: () => Konva.Stage) {
+    setContext(stageKey, getStage());
+}
+
+export function getStageContext() {
+    
+    const getStage = getContext<() => Konva.Stage>(stageKey);
+    if(!getStage) {
+        throw new Error('A layer must have a Stage as a parent.')
+    }
+    const stage = getStage();
+    return stage;
+}
